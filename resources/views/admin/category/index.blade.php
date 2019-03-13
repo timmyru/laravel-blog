3072fe77979b4c6aef1c87f28dfcef1088cd172d
Artem <aprygin@mail.ru>
2019-03-13 09:27:56 +0700

    'next'

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,38 @@
+@extends ('admin.layouts.app_admin')
+@section('content')
+    <div class="container">
+        @component('admin.components.breadcrumps')
+            @slot('title') Список категорий @endslot
+            @slot('parent') Главная @endslot
+            @slot('active') Категории @endslot
+        @endcomponent
+
+        <hr>
+
+        <a href="{{route('admin.category.create')}}" class="btn btn-primary pull-right"><i class="fa fa-plus-square-o" style="margin-right: 5px; vertical-align: middle"></i>
+        <span style="vertical-align: middle">Создать категорию</span></a>
+
+        <table class="table table-striped">
+            <thead>
+            <th>Наименование</th>
+            <th>Публикация</th>
+            <th class="text-right">Действие</th>
+            </thead>
+            <tbody>
+                @forelse($categories as $category)
+                    <tr>
+                        <td>{{$category->title}}</td>
+                        <td>{{$category->published}}</td>
+                        <a href="{{route('admin.category.edit', ['id'=>$category->id])}}"><i class="fa fa-edit"></i></a>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="3" class="text-center">
+                            <h2>Данные отсутствуют</h2>
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+@endsection
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,36 +5,36 @@
         <div class="row">
             <div class="col-sm-3">
                 <div class="jumbotron">
-                    <p><span class="label label-primary">Категорий 0</span></p>
+                    <h4><span class="badge badge-primary">Категорий 0</span></h4>
                 </div>
             </div>
             <div class="col-sm-3">
                 <div class="jumbotron">
-                    <p><span class="label label-primary">Материалов 0</span></p>
+                    <h4><span class="badge badge-primary">Материалов 0</span></h4>
                 </div>
             </div>
             <div class="col-sm-3">
                 <div class="jumbotron">
-                    <p><span class="label label-primary">Посетителей 0</span></p>
+                    <h4><span class="badge badge-primary">Посетителей 0</span></h4>
                 </div>
             </div>
             <div class="col-sm-3">
                 <div class="jumbotron">
-                    <p><span class="label label-primary">Сегодня 0</span></p>
+                    <h4><span class="badge badge-primary">Сегодня 0</span></h4>
                 </div>
             </div>
         </div>
 
         <div class="row">
             <div class="col-sm-6">
-                <a href="" class="btn btn-block btn-default">Создать категорию</a>
+                <a href="" class="btn btn-block btn-secondary">Создать категорию</a>
                 <a href="#" class="list-group-item">
                     <h4 class="list-group-item-heading">Категория первая</h4>
                     <p class="list-group-item-text">Количество материалов</p>
                 </a>
             </div>
             <div class="col-sm-6">
-                <a href="" class="btn btn-block btn-default">Создать материал</a>
+                <a href="" class="btn btn-block btn-secondary">Создать материал</a>
                 <a href="#" class="list-group-item">
                     <h4 class="list-group-item-heading">Материал первый</h4>
                     <p class="list-group-item-text">Категория</p>
